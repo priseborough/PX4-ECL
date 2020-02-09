bca23f5c9d620da438d673d86df421276010ca64
Paul Riseborough <p_riseborough@live.com.au>
2020-02-09 13:28:12 +1100

    EKF: EKF-GSF use const type and update comments

--- EKF/EKFGSF_yaw_estimator.cpp
+++ EKF/EKFGSF_yaw_estimator.cpp
@@ -220,16 +220,17 @@ void Ekf::statePredictEKFGSF(uint8_t model_index)
 
 	// predict covariance - autocode from https://github.com/priseborough/3_state_filter/blob/flightLogReplay-wip/calcPupdate.txt
 
-	// local copy required
-	float P00 = _ekf_gsf[model_index].P[0][0];
-	float P01 = _ekf_gsf[model_index].P[0][1];
-	float P02 = _ekf_gsf[model_index].P[0][2];
-	float P10 = _ekf_gsf[model_index].P[1][0];
-	float P11 = _ekf_gsf[model_index].P[1][1];
-	float P12 = _ekf_gsf[model_index].P[1][2];
-	float P20 = _ekf_gsf[model_index].P[2][0];
-	float P21 = _ekf_gsf[model_index].P[2][1];
-	float P22 = _ekf_gsf[model_index].P[2][2];
+	// Local short variable name copies required for readability
+	// Compiler might be smart enough to optimise these out
+	const float P00 = _ekf_gsf[model_index].P[0][0];
+	const float P01 = _ekf_gsf[model_index].P[0][1];
+	const float P02 = _ekf_gsf[model_index].P[0][2];
+	const float P10 = _ekf_gsf[model_index].P[1][0];
+	const float P11 = _ekf_gsf[model_index].P[1][1];
+	const float P12 = _ekf_gsf[model_index].P[1][2];
+	const float P20 = _ekf_gsf[model_index].P[2][0];
+	const float P21 = _ekf_gsf[model_index].P[2][1];
+	const float P22 = _ekf_gsf[model_index].P[2][2];
 
 	// Use fixed values for delta velocity and delta angle process noise variances
 	const float dvxVar = sq(_params.EKFGSF_accel_noise * _imu_sample_delayed.delta_vel_dt); // variance of forward delta velocity - (m/s)^2
