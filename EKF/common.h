85083e73075c37ecdfa3fe829153065eb37d78f8
Paul Riseborough <p_riseborough@live.com.au>
2020-03-05 10:53:52 +1100

    EKF: Improve AHRS accuracy during dynamic flight profiles.

--- EKF/common.h
+++ EKF/common.h
@@ -365,8 +365,8 @@ struct parameters {
 	// Parameters used by EKF-GSF yaw estimator
 	float EKFGSF_gyro_noise{1.0e-1f}; 	///< yaw rate noise used for covariance prediction (rad/sec)
 	float EKFGSF_accel_noise{2.0f};		///< horizontal accel noise used for covariance prediction (m/sec**2)
-	float EKFGSF_tilt_gain{0.4f};		///< gain from tilt error to gyro correction for complementary filter (1/sec)
-	float EKFGSF_gyro_bias_gain{0.04f};	///< gain applied to integral of gyro correction for complementary filter (1/sec)
+	float EKFGSF_tilt_gain{0.2f};		///< gain from tilt error to gyro correction for complementary filter (1/sec)
+	float EKFGSF_gyro_bias_gain{0.02f};	///< gain applied to integral of gyro correction for complementary filter (1/sec)
 	float EKFGSF_weight_min{0.0f};		///< minimum value of an individual model weighting
 	float EKFGSF_tas_default{15.0f};	///< default airspeed value assumed during fixed wing flight if no airspeed measurement available (m/s)
 	unsigned EKFGSF_reset_delay{1000000};	///< Number of uSec of bad innovations on main filter inpost takeoff phase before yaw is reset to EKF-GSF value
