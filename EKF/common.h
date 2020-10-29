f50c68ba8e6f90003d19b4cf35ebcc3e6b86f566
Paul Riseborough <gncsolns@gmail.com>
2020-10-30 08:04:48 +1100

    EKF: Replace Matlab auto generated code with SymPy output
    Increases stability of covariance prediction.

--- EKF/common.h
+++ EKF/common.h
@@ -472,4 +472,21 @@ union ekf_solution_status {
 	uint16_t value;
 };
 
+namespace ecl{
+	inline float powf(float x, int exp)
+	{
+		float ret;
+		if (exp > 0) {
+			ret = x;
+			for (int count = 1; count < exp; count++) {
+				ret *= x;
+			}
+			return ret;
+		} else if (exp < 0) {
+			return 1.0f / ecl::powf(x, -exp);
+		}
+		return 1.0f;
+	}
+}
+
 }
